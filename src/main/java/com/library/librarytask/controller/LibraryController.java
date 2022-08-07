ac45a5ba7c6a3baed80721421079f9a1bd1144f5
Piotr Tynek <tynek.piotr@gmail.com>
2022-08-07 15:18:34 +0200

    Initial commit

---
+++ src/main/java/com/library/librarytask/controller/LibraryController.java
@@ -0,0 +1,7 @@
+package com.library.librarytask.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LibraryController {
+}
---
+++ src/test/java/com/library/librarytask/LibraryTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.librarytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
